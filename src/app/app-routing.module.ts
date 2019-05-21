21206671ef970037a4af26f985f3635fd193cad4
brendan richards <brendan_richards_msdn@hotmail.com>
2019-05-22 08:46:59 +1000

    added routes and edit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CompanyListComponent } from './company/company-list/company-list.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'company/list', pathMatch: 'full'  },
+  { path: 'company/list', component: CompanyListComponent },
+  { path: 'company/edit', component: CompanyEditComponent  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { FormsModule } from '@angular/forms';
 import { CompanyListComponent } from './company/company-list/company-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompanyListComponent
+    CompanyListComponent,
+    CompanyEditComponent
   ],
   imports: [
     BrowserModule,
