386cbdabc90c888ea53e130b396ae59ffe00057f
brendan richards <brendan_richards_msdn@hotmail.com>
2019-05-21 14:58:07 +1000

    added environment config

--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Company } from './company';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class CompanyService {
     private httpClient: HttpClient
   ) { }
 
-  API_BASE = 'http://firebootcamp-crm-api.azurewebsites.net/api';
+  API_BASE = environment.API_BASE;
 
   getCompanies(): Observable<Company[]> {
     return this.httpClient.get<Company[]>(`${this.API_BASE}/company`);
