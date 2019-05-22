057640c9f12850655914aa9e87a78438bba9dfce
SSWTemp <SSWTemp@ssw.com.au>
2019-05-22 13:00:22 +1000

    added company count in stateless mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CompanyService } from './company/company.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'fbc-root',
@@ -8,4 +11,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'firebootcamp-crm';
 
+  companiesCount$: Observable<number>;
+
+  constructor(
+    private companyService: CompanyService 
+  ){}
+   
+  ngOnInit() {
+    this.companiesCount$ = this.companyService.getCompanies()
+    .pipe(map(list => list.length));
+  }
 }
